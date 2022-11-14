ae865a920862ab1628eec70a78d734f7e2cdd51c
ifeday1 <oladeniifeoluwa123@gmail.com>
2022-11-14 10:18:24 +0100

    working on project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PageWrapper from './components/wrapper/PageWrapper';
+import Homepage from './pages/Homepage';
+import PlaceToStay from './pages/PlaceToStay';
 
 function App() {
   return (
-    <div className="App">
-       <h1 className=' bg-black text-xs text-white  '>Ife</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<PageWrapper><Homepage /></PageWrapper>} />
+        <Route path="/place-to-stay" exact element={<PageWrapper><PlaceToStay /></PageWrapper>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
